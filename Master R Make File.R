2e308840e914c412baa63563132230bbf3665b8d
dmf42186 <freemand@smu.edu>
2017-04-10 19:54:07 -0400

    Add files via upload

---
+++ Master R Make File.R
@@ -0,0 +1,22 @@
+#Dan Freeman
+#Apirl 10, 2017
+#MSDS 6306 Doing Data Science
+#Case Study 1
+
+#This file sources syntax from other .R files that import, clean, merge and
+#analyze the data.
+
+#REMEMBER TO SET YOUR WORKING DIRECTORY!
+setwd("C:/Users/Dan Freeman/Documents/Southern Methodist University - MS in Data Science/Spring 2017 Courses/MSDS 6306 Doing Data Science/Case Study 1")
+
+#Import the data.
+source("./Case-Study-1/Analysis/Data Import.R ")
+
+#Clean the data.
+source("./Case-Study-1/Analysis/Data Clean.R ")
+
+#Merge the data.
+source("./Case-Study-1/Analysis/Data Merge.R ")
+
+#Analyze the data.
+source("./Case-Study-1/Analysis/Data Analysis.R ")
