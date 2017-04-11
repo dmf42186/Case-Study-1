3797e82c05e7d7e8e0c355cab7e3814d8fcd70b0
dmf42186 <freemand@smu.edu>
2017-04-11 01:22:46 -0400

    Add files via upload

--- Master R Make File.R
+++ Master R Make File.R
@@ -6,17 +6,24 @@
 #This file sources syntax from other .R files that import, clean, merge and
 #analyze the data.
 
+
+# Packages Needed
+#install.packages("plyr")
+#install.packages("ggplot2")
+library(plyr)
+library(ggplot2)
+
 #REMEMBER TO SET YOUR WORKING DIRECTORY!
 setwd("C:/Users/Dan Freeman/Documents/Southern Methodist University - MS in Data Science/Spring 2017 Courses/MSDS 6306 Doing Data Science/Case Study 1")
 
 #Import the data.
-source("./Case-Study-1/Analysis/Data Import.R ")
+source("./Analysis/Data Imports.R ")
 
 #Clean the data.
-source("./Case-Study-1/Analysis/Data Clean.R ")
+source("./Analysis/Data Cleaning.R ")
 
 #Merge the data.
-source("./Case-Study-1/Analysis/Data Merge.R ")
+source("./Analysis/Data Merge.R ")
 
 #Analyze the data.
-source("./Case-Study-1/Analysis/Data Analysis.R ")
+source("./Analysis/Data Analysis.R ")
